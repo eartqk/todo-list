447b84066be8d9ae446f9ce24f63701a3b73c5e3
eartqk <eartqk@gmail.com>
2021-03-15 23:28:59 +0300

    added overview of methods

--- backend/views.py
+++ backend/views.py
@@ -9,6 +9,19 @@ from .models import Task
 
 
 @api_view(['GET'])
+def apiOverview(request):
+    api_urls = {
+        'List':'/task-list/',
+		'Detail view':'/task-detail/<str:pk>/',
+		'Create':'/task-create/',
+		'Update':'/task-update/<str:pk>/',
+		'Delete':'/task-delete/<str:pk>/',
+    }
+
+    return Response(api_urls)
+
+
+@api_view(['GET'])
 def taskList(request):
     tasks = Task.objects.all().order_by('id')
     serializer = TaskSerializer(tasks, many=True)    
