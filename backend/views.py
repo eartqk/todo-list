d79f178d9b0babea2f6b7d171651dbe88ed62c35
eartqk <eartqk@gmail.com>
2021-03-15 23:24:42 +0300

    added methods

--- backend/views.py
+++ backend/views.py
@@ -11,6 +11,41 @@ from .models import Task
 @api_view(['GET'])
 def taskList(request):
     tasks = Task.objects.all().order_by('id')
-    serializer = TaskSerializer(tasks, many=True)
+    serializer = TaskSerializer(tasks, many=True)    
     
     return Response(serializer.data)
+
+@api_view(['GET'])
+def taskDetail(request, pk):
+    task = Task.objects.get(id=pk)
+    serializer = TaskSerializer(task, many=False)
+
+    return Response(serializer.data)
+
+
+@api_view(['POST'])
+def taskCreate(request):
+    serializer = TaskSerializer(data=request.data)
+
+    if serializer.is_valid():
+        serializer.save()
+
+    return Response(serializer.data)
+
+@api_view(['POST'])
+def taskUpdate(request, pk):
+    task = Task.objects.get(id=pk)
+    serializer = TaskSerializer(instance=task, data=request.data)
+
+    if serializer.is_valid():
+        serializer.save()
+
+    return Response(serializer.data)
+
+
+@api_view(['DELETE'])
+def taskDelete(request, pk):
+    task = Task.objects.get(id=pk)
+    task.delete()
+
+    return Response("Item deleted!")
